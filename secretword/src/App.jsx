e297a80a3a6741dadb8ed0d3c917a6d7f0c74ac0
Vilardo22 <gabrielvilardo22@gmail.com>
2023-08-26 15:27:57 -0300

    Agora os botoes estao com a funcionalidades corretas podendo navegar entre as pages

--- secretword/src/App.jsx
+++ secretword/src/App.jsx
@@ -1,13 +1,51 @@
-import { useState } from 'react'
+
+//Css
 import './App.css'
+
+//React
+import { useCallback, useEffect, useState } from 'react';
+
+
+//data
+import {wordsList} from "./data/Word";
+
+
+
+// components
 import { StartScreen } from './components/StartScreen'
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages =[
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"}
+];
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [word] = useState(wordsList)
+
+  // start the secret word game
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  };
+
+  // process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  }
+  // restarts the game
+  const retry = () =>{
+    setGameStage(stages[0].name)
+  }
 
   return (
     <div className='App'>
-      <StartScreen />
+      {gameStage === "start" && <StartScreen startGame= {startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry}  />}
     </div>
       
     
--- secretword/src/components/StartScreen.jsx
+++ secretword/src/components/StartScreen.jsx
@@ -1,11 +1,11 @@
 import "./StartScreen.css";
 
-export const StartScreen = () => {
+export const StartScreen = ({startGame}) => {
   return (
     <div className="start">
         <h1>Secret Word</h1>
         <p>Clique no Botão abaixo para começar a jogar</p>
-        <button>Comecar o jogo</button>
+        <button onClick={startGame}>Comecar o jogo</button>
     </div>
   )
 }
