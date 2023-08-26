5ed20691832b75d753f1afed4ac16d806a19b6d2
Vilardo22 <gabrielvilardo22@gmail.com>
2023-08-26 13:50:32 -0300

    adcionado os botoes

--- secretword/src/App.jsx
+++ secretword/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import { StartScreen } from './components/StartScreen'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
-      <h2>Secret Word</h2>
+      <StartScreen />
     </div>
       
     
   
-)
+  )
 }
 export default App
---
+++ secretword/src/components/StartScreen.jsx
@@ -0,0 +1,11 @@
+import "./StartScreen.css";
+
+export const StartScreen = () => {
+  return (
+    <div className="start">
+        <h1>Secret Word</h1>
+        <p>Clique no Botão abaixo para começar a jogar</p>
+        <button>Comecar o jogo</button>
+    </div>
+  )
+}
